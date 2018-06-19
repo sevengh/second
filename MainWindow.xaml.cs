8ba4947f1bae2a6fba8bf8d8f7f2fd995bac1f62
Sergey Shamanayev <shamanayev.sergey@gmail.com>
2018-06-19 16:26:54 +0400

    Implement application to show random color window

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Threading;
+
+namespace Second
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        DispatcherTimer dispatcherTimerGoMainMenu = new DispatcherTimer();
+        Random random = new Random();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            dispatcherTimerGoMainMenu.Tick += new EventHandler(DispatcherTimer_Tick);
+            dispatcherTimerGoMainMenu.Interval = new TimeSpan(0, 0, 10);
+            dispatcherTimerGoMainMenu.IsEnabled = true;
+
+            ChangeBackgrdound();
+        }
+
+        private void DispatcherTimer_Tick(object sender, EventArgs e)
+        {
+            ChangeBackgrdound();
+        }
+
+        void ChangeBackgrdound()
+        {
+            Window1.Background =
+                new SolidColorBrush(
+                    Color.FromRgb(
+                    (byte)random.Next(255),
+                    (byte)random.Next(255),
+                    (byte)random.Next(255)
+                    ));
+        }
+    }
+}
